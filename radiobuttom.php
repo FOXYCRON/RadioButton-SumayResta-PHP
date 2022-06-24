b6c757bb397a972b30f6dd696627e6e9b6b2df6e
FOXYCRON <57571091+FOXYCRON@users.noreply.github.com>
2022-06-24 12:26:25 -0600

    Add files via upload

---
+++ radiobuttom.php
@@ -0,0 +1,30 @@
+<html>
+   <head>
+      <title> RadioButtom </title>
+   </head>
+   <body>
+
+     <Form method = "post" action = "RadioPagina2.php">
+     <p> Ingrese Primer Valor: </p>
+     <input type = "text" name = "valor1">
+
+     <br>
+
+     <p> Ingrese Segundo Valor: </p>
+     <input type = "text" name = "valor2">
+     
+     <br>
+
+     <input type = "radio" name = "radio1" value = "suma">
+  Sumar
+     <br>
+     
+     <input type = "radio" name = "radio1" value = "resta">
+  Restar
+     <br>
+
+    <input type = "submit" name = "Enviar">
+ 
+   </form>
+ </body>
+</html>
